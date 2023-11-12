4c83f676cdb287601547ca1a00845a8f99b2c206
Naoya <33260607+naoya-o-git@users.noreply.github.com>
2023-11-12 17:09:51 +0000

    Update 9 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import "./styles.css";
+import React, { useEffect, useState } from "react";
+import { ColorfulMessage } from "./components/ColorfulMessage";
 
-export default function App() {
+const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <h1 style={{ color: "red" }}>こんにちは!</h1>
+      <ColorfulMessage color="blue">お元気ですか?</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です!</ColorfulMessage>
+      <button onClick={onClickCountUp}>カウントアップ!</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>(^^)</p>}
+    </>
   );
-}
+};
+
+export default App;
